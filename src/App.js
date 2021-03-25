70b43a6016cfbd03add7a54b067defe3ae22dbcb
Gonchi Monchi <57135888+GonchiMonchi@users.noreply.github.com>
2021-03-25 18:15:01 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Si leeis esto que sepais que sois muy guapos <3
         </p>
         <a
           className="App-link"
